7c7ad78fbb1bda644b36c18828a06dee80053156
kaviyanjali17 <kaviyanjali17@gmail.com>
2025-08-28 16:40:22 +0530

    lab9

---
+++ Lab9_q1_Testng.java
@@ -0,0 +1,84 @@
+package pac1;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Lab9_q1_Testng {
+    WebDriver driver;
+
+    @Parameters("browser")
+    @BeforeMethod
+    public void beforeMethod(String brow) {
+        System.out.println("This is Before Method");
+
+        if (brow.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+        if (brow.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        }
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+
+      
+        driver.get("https://tutorialsninja.com/demo/index.php?");
+        System.out.println("Opened TutorialNinja demo site.");
+    }
+
+    @Test
+    public void testLab4Flow() throws InterruptedException {
+        String title = driver.getTitle();
+        System.out.println("Page Title: " + title);
+
+        driver.findElement(By.linkText("Desktops")).click();
+        driver.findElement(By.linkText("Mac (1)")).click();
+        System.out.println("Clicked on Mac under Desktops.");
+
+        WebElement macHeading = driver.findElement(By.xpath("/html/body/div[2]/div/div/h2"));
+        Assert.assertTrue(macHeading.isDisplayed(), "Mac heading is not displayed!");
+        System.out.println("Verified Mac heading is displayed.");
+
+        Select sort = new Select(driver.findElement(By.id("input-sort")));
+        sort.selectByVisibleText("Name (A - Z)");
+        System.out.println("Selected Sort By: Name (A-Z)");
+
+        driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/div/div/div[2]/div[2]/button[1]/i")).click();
+        System.out.println("Clicked on Add to Cart for first product.");
+
+        WebElement searchBox = driver.findElement(By.name("search"));
+        searchBox.clear();
+        searchBox.sendKeys("Mobile");
+        driver.findElement(By.xpath("/html/body/header/div/div/div[2]/div/span/button")).click();
+        System.out.println("Searched for 'Mobile'.");
+
+        WebElement searchCriteria = driver.findElement(By.name("search"));
+        searchCriteria.clear();
+        System.out.println("Cleared Search criteria.");
+
+        driver.findElement(By.name("description")).click();
+        driver.findElement(By.id("button-search")).click();
+        System.out.println("Clicked Search with 'Search in product descriptions' checked.");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+            System.out.println("Browser closed.");
+        }
+    }
+}
